d239442a7db32f36f4c6b8e6baa75f4ab7a395ac
MaksymMartseniuk <marcenukm995@gmail.com>
2026-04-17 17:18:24 +0300

    feat(api): add request interceptor for authorization and define public endpoints

--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { tokenService } from './token.service';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -8,5 +9,28 @@ const api = axios.create({
         'Content-Type': 'application/json',
     },
 });
+
+const PUBLIC_ENDPOINTS=['/api/user/register',
+    '/api/user/login',
+    '/api/user/refresh-token/',
+    '/api/user/forget-password/',
+    '/api/user/reset-password/',
+    '/api/user/verify-email/'];
+
+api.interceptors.request.use(
+    (config) => {
+        const isPiblicEndpoint = PUBLIC_ENDPOINTS.some(endpoint => config.url?.includes(endpoint));
+        if (!isPiblicEndpoint) {
+            const accessToken = tokenService.getAccessToken();
+            if (accessToken) {
+                config.headers['Authorization'] = `Bearer ${accessToken}`;
+            }
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
